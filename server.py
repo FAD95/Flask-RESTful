94df9e4767ea8cb3447c7ecacaa9cbd195564706
Fad <feescorciam@protonmail.com>
2021-10-06 14:15:29 -0500

    another route added

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_restful import Resource, Api, reqparse, abort
 
 app = Flask(__name__)
@@ -18,20 +18,38 @@ class Todo(Resource):
     
     def get(self, todo_id):
         abort_if_todo_doesnt_exist(todo_id)
-        return TODOS[todo_id]
+        return jsonify(TODOS[todo_id])
 
     def put(self, todo_id):
         args = parser.parse_args()
         TODOS[todo_id] = {"task": args["task"], "time": args["time"]}
-        return jsonify({todo_id: TODOS[todo_id]})
+        return make_response(jsonify({todo_id: TODOS[todo_id]}), 201)
     
     def delete(self, todo_id):
         abort_if_todo_doesnt_exist(todo_id)
         del TODOS[todo_id]
+        return make_response(jsonify(TODOS), 204)
+
+class Todos(Resource):
+    def get(self):
         return jsonify(TODOS)
+
+    def post(self, todo_id):
+        args = parser.parse_args()
+        if todo_id not in TODOS:
+            TODOS[todo_id] = {"task": args["task"], "time": args["time"]}
+            return make_response(jsonify({todo_id: TODOS[todo_id]}), 201)
+        else:
+            abort(400, message = f"Task {todo_id} already exists")
+
 
 api.add_resource(Todo, 
     '/<string:todo_id>',
+    '/todo/<string:todo_id>'
+)
+
+api.add_resource(Todos, 
+    '/todos',
     '/todos/<string:todo_id>'
 )
 
